7aec4f883a8ee45ebbd906c619f5559efbaa1b16
JaeSeungJae <valkyrie9923@gmail.com>
2024-03-16 02:26:01 +0900

    03.15

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+    return (
+        <Card title="abc" backgroundColor="#4ea04e">
+            <p>안녕하세요.</p>
+            <p>저는 ㅁㅁㅁㅁㅁㅁ.</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
